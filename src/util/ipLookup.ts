a1d7a4c5b1f32696c9820a0e1769e91b9f90f7aa
jork41989 <jork41989@gmail.com>
2021-02-11 11:18:35 -0500

    adjusting the header

--- src/util/ipLookup.ts
+++ src/util/ipLookup.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+axios.defaults.headers.get["User-Agent"] = "keycdn-tools:https?://.*"
 export const getLocation = (ipAddress: string) => {
   console.log(ipAddress)
   return axios.get(`https://tools.keycdn.com/geo.json?host=${ipAddress}`)
