6078b2c8bf2d76d6328116c07aceaf6b6b386f02
jork41989 <jork41989@gmail.com>
2021-02-11 10:05:26 -0500

    added public-ip to find the users ip address

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import IpCheck from './components/ip_finder/ipFinder.tsx';
 
 function App() {
   return (
     <div className="App">
       <div>Hello World</div>
+      <IpCheck/>
     </div>
   );
 }
--- src/components/ip_finder/ipFinder.tsx
+++ src/components/ip_finder/ipFinder.tsx
@@ -1,9 +1,17 @@
+const publicIp = require('public-ip');
 
-export default function ipCheck(){
-
+export default function IpCheck(){
+  let getClientIp = async () => await publicIp.v4({
+  fallbackUrls: [ "https://ifconfig.co/ip" ]
+});
+  let ipLogger = () => {
+    console.log(getClientIp())
+  }
+  
   return(
     <div>
       Hello World
+      {ipLogger()}
     </div>
   )
 }
